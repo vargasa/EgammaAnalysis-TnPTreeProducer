e5f59052cc31a32100d236b2f9efbce85cd6f1c6
tomcornelis <tomc@cern.ch>
2020-05-19 09:43:43 +0200

    For UL2017 another JSON? In good CMS tradition I can't find documentation about it, but I guess so...

--- crab/tnpCrabSubmit.py
+++ crab/tnpCrabSubmit.py
@@ -42,7 +42,7 @@ config.Site.storageSite                = 'T2_CH_CERN'
 #
 def getLumiMask(era):
   if   era=='2016': return 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
-  elif era=='2017': return 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'
+  elif era=='2017': return 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'
   elif era=='2018': return 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
 
 
